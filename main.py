111d98dfe01bd2e4f5c88ccb103292e1365ad93b
Jan <janekrembikowski@gmail.com>
2023-01-12 17:23:39 +0100

    added graphic fixes and heatmap

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 
 # importowanie csv do ramki danych
 patients = pd.read_csv('data/cancer_patient_data_sets.csv')
+patients=patients.replace(to_replace="Low", value=1)
+patients=patients.replace(to_replace="Medium", value=2)
+patients=patients.replace(to_replace="High", value=3)
 print(patients.head())
 print("Data imported correctly.")
 
@@ -21,14 +24,44 @@ print(patients.isnull().values.any())
 
 # rozkład płci
 gender_dist = patients['Gender'].hist(bins=2, label=["Female", "Male"])
+plt.xlabel("gender")
+plt.ylabel("number of people")
 title = 'GENDER DISTRIBUTION'
 gender_dist.set_title(title)
 plt.gca().set_xticklabels(['Male', 'Female'])
 plt.xticks(np.arange(min(patients['Gender']), max(patients['Gender']) + 1, 1))
 plt.show()
 
-# ryzyko wystąpienia raka płuc
-cancer_risk = patients['Level'].hist(bins=3)
-title = 'RISK OF DEVELOPING LUNG CANCER'
-gender_dist.set_title(title)
+# palacze w grupie badawczej
+cancer_risk = patients['Smoking'].hist(bins=8)
+title = 'SMOKERS IN THE RESEARCH GROUP'
+plt.xlabel("level of smoking")
+plt.ylabel("number of people")
+cancer_risk.set_title(title)
 plt.show()
+
+# rozkład danych z uwzglednieniem wieku
+age_dist = patients['Age'].plot.box()
+title = 'DATA DISTRIBUTION (BASED ON AGE)'
+age_dist.set_title(title)
+plt.show()
+
+# usuwanie nieprzydatnych danych
+patients.drop(['index', 'Patient Id'], axis=1, inplace = True)
+print(patients.head())
+print('Unneccesary data deleted!')
+
+# wyznaczenie macierzy korelacji
+
+correlations = patients.corr()
+fig, ax = plt.subplots(figsize=(24, 24))
+
+colormap = sns.color_palette("BrBG", 10)
+
+sns.heatmap(correlations,
+    cmap=colormap,
+    annot=True,
+    fmt=".2f")
+plt.show()
+
+
