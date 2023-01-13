5357199342898dc97ff79997fe25221511691160
arix2000 <arix20003@gmail.com>
2023-01-13 21:35:02 +0100

    add prediction menu

--- utils/ml_model_use_case.py
+++ utils/ml_model_use_case.py
@@ -8,7 +8,21 @@ class MLModelUseCase:
         self.y_train = y_train
 
     def start(self):
-        new_patient = np.array([51, 4, 2, 4, 5, 5, 7, 7])
+        age = input("Enter patient age: ")
+        gender = input("Enter gender (male/female)")
+        if gender == "male":
+            gender = 1
+        else:
+            gender = 2
+        air_pollution = input("What is the air pollution at the patient's place of residence? (1-7) ")
+        alcohol_use = input("How much alcohol the patient is using? (1-7) ")
+        dust_allergy = input("Enter the level of dust allergy of the patient. (1-7) ")
+        smoking = input("How much does the patient smoke? (1-7) ")
+        passive_smoker = input("How often the patient is around smokers? (1-7) ")
+        snoring = input("How often does the patient snore? (1-7) ")
+
+        new_patient = np.array(
+            [age, gender, air_pollution, alcohol_use, dust_allergy, smoking, passive_smoker, snoring])
         new_patient = new_patient.reshape(1, -1)
         self.predict_if_patient_has_lung_cancer(new_patient)
 
